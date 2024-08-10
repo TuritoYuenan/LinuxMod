97dc77c588b9f7879da35b7f066c18e07ece2e43
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2024-08-10 09:59:31 +0700

    random refactorings

--- src/main/java/vn/turito/minefetch/Minefetch.java
+++ src/main/java/vn/turito/minefetch/Minefetch.java
@@ -3,39 +3,50 @@ package vn.turito.minefetch;
 import com.mojang.brigadier.Command;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
 
-import static net.minecraft.server.command.CommandManager.literal;
-
+/**
+ * Main class for Minefetch mod
+ */
 class Minefetch implements ModInitializer {
 	Command<ServerCommandSource> fetchCommand = context -> {
 		ServerCommandSource source = context.getSource();
-
 		ServerPlayerEntity player = source.getPlayer();
-		String positions = player.getBlockX()+", "+player.getBlockY()+", "+player.getBlockZ();
 
 		String[] lines = {
-			"██████████  " + player.getDisplayName() + "@" + source.getServer(),
-			"██░░██░░██  " + "Seed: " + source.getWorld().getSeed(),
-			"████░░████  " + "Time: " + source.getWorld().getTime(),
-			"███░░░░███  " + "Position: " + positions,
-			"███░██░███  ",
-			"██████████  ",
+			"mmmmmmmmmm  " + player.getDisplayName() + "@" + source.getServer(),
+			"mmccmmccmm  " + "Seed: " + source.getWorld().getSeed(),
+			"mmmmccmmmm  " + "Time: " + source.getWorld().getTime(),
+			"mmmccccmmm  " + "Position: " + getPlayerPosition(player),
+			"mmmcmmcmmm  ",
+			"mmmmmmmmmm  ",
 		};
 
-		for (String i: lines) {
+		for (String i : lines) {
 			source.sendFeedback(new LiteralText(i), false);
 		}
 
 		return 0;
 	};
 
+	/**
+	 * @param p Object representing the player
+	 * @return Formatted string of the player's position
+	 */
+	String getPlayerPosition(ServerPlayerEntity p) {
+		String position = "%d, %d, %d";
+		return String.format(position, p.getBlockX(), p.getBlockY(), p.getBlockZ());
+	}
+
 	@Override
 	public void onInitialize() {
 		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) ->
-			dispatcher.register(literal("minefetch").executes(this.fetchCommand))
+			dispatcher.register(
+				CommandManager.literal("minefetch").executes(this.fetchCommand)
+			)
 		);
 	}
 }
