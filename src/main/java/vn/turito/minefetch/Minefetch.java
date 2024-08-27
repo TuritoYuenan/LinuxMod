670b0204a1f2509513a0b7e3b55efc9d751e5d3e
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2024-08-27 20:05:06 +0700

    migrate to 1.21.1

--- src/main/java/vn/turito/minefetch/Minefetch.java
+++ src/main/java/vn/turito/minefetch/Minefetch.java
@@ -1,32 +1,41 @@
 package vn.turito.minefetch;
 
-import com.mojang.brigadier.Command;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.LiteralText;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
 
-/**
- * Main class for Minefetch mod
- */
-class Minefetch implements ModInitializer {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+
+public class Minefetch implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("minefetch");
+
 	Command<ServerCommandSource> fetchCommand = context -> {
 		ServerCommandSource source = context.getSource();
 		ServerPlayerEntity player = source.getPlayer();
+		ServerWorld world = source.getWorld();
 
 		String[] lines = {
-			"mmmmmmmmmm  " + player.getDisplayName() + "@" + source.getServer(),
-			"mmccmmccmm  " + "Seed: " + source.getWorld().getSeed(),
-			"mmmmccmmmm  " + "Time: " + source.getWorld().getTime(),
-			"mmmccccmmm  " + "Position: " + getPlayerPosition(player),
-			"mmmcmmcmmm  ",
-			"mmmmmmmmmm  ",
+				"mmmmmmmmmm  " + player.getDisplayName() + "@" + source.getServer(),
+				"mmccmmccmm  " + "Seed: " + world.getSeed(),
+				"mmmmccmmmm  " + "Time: " + world.getTime(),
+				"mmmccccmmm  " + "Position: " + getPlayerPosition(player),
+				"mmmcmmcmmm  ",
+				"mmmmmmmmmm  ",
 		};
 
-		for (String i : lines) {
-			source.sendFeedback(new LiteralText(i), false);
+		for (String line : lines) {
+			source.sendFeedback(() -> Text.literal(line), false);
 		}
 
 		return 0;
@@ -36,17 +45,25 @@ class Minefetch implements ModInitializer {
 	 * @param p Object representing the player
 	 * @return Formatted string of the player's position
 	 */
-	String getPlayerPosition(ServerPlayerEntity p) {
+	private static String getPlayerPosition(ServerPlayerEntity p) {
 		String position = "%d, %d, %d";
 		return String.format(position, p.getBlockX(), p.getBlockY(), p.getBlockZ());
 	}
 
 	@Override
 	public void onInitialize() {
-		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) ->
-			dispatcher.register(
-				CommandManager.literal("minefetch").executes(this.fetchCommand)
-			)
-		);
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		LOGGER.info("Minefetch mod activated!");
+
+		LiteralArgumentBuilder<ServerCommandSource> command = CommandManager
+				.literal("minefetch")
+				.executes(this.fetchCommand);
+
+		CommandRegistrationCallback.EVENT
+				.register((dispatcher, registryAccess, environment) -> {
+					dispatcher.register(command);
+				});
 	}
 }
